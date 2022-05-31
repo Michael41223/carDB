64785f6b43b98d620638a5a6dcc8deb673626c7a
MichaelMaerean <83560055+Michael41223@users.noreply.github.com>
2022-05-31 22:44:15 +0000

    finished photos

--- models.py
+++ models.py
@@ -4,9 +4,9 @@ class Cars(db.Model):
     __tablename__ = 'Cars'
 
     id = db.Column(db.Integer, primary_key=True)
-    make = db.Column(db.String(80))
-    model = db.Column(db.String(80))
-    year = db.Column(db.Integer)
-    description = db.Column(db.String())
-    carimage = db.Column(db.String(80))
-    engineimage = db.Column(db.String(80))
+    make = db.Column(db.String(50))
+    model = db.Column(db.String(50))
+    year = db.Column(db.String(10))
+    engine = db.Column(db.String(50))
+    carimage = db.Column(db.Integer)
+    engineimage = db.Column(db.Integer)
